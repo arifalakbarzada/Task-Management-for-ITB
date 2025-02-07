49fe3ba65729764ecccd8b4c532ec1813e5d10f9
arif2004js <yearif689@gmail.com>
2025-02-07 14:31:47 +0400

    REdux changed to redux toolkit

--- src/components/AppSidebar.jsx
+++ src/components/AppSidebar.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import {
   CCloseButton,
   CSidebar,
@@ -8,33 +7,39 @@ import {
   CSidebarFooter,
   CSidebarHeader,
   CSidebarToggler,
-} from '@coreui/react'
-import CIcon from '@coreui/icons-react'
+} from '@coreui/react';
+import CIcon from '@coreui/icons-react';
 
-import { AppSidebarNav } from './AppSidebarNav'
+import { AppSidebarNav } from './AppSidebarNav';
+import { logo } from 'src/assets/brand/logo';
+import { sygnet } from 'src/assets/brand/sygnet';
+import navigation from '../_nav';
 
-import { logo } from 'src/assets/brand/logo'
-import { sygnet } from 'src/assets/brand/sygnet'
-
-import navigation from '../_nav'
+import { setState } from '../store';
 
 const AppSidebar = () => {
-  const dispatch = useDispatch()
-  const unfoldable = useSelector((state) => state.sidebarUnfoldable)
-  const sidebarShow = useSelector((state) => state.sidebarShow)
+  const dispatch = useDispatch();
+  const sidebarShow = useSelector((state) => state.app.sidebarShow);
+  const sidebarUnfoldable = useSelector((state) => state.app.sidebarUnfoldable);
+
+  const toggleSidebar = (visible) => {
+    dispatch(setState({ sidebarShow: visible }));
+  };
+
+  const toggleUnfoldable = () => {
+    dispatch(setState({ sidebarUnfoldable: !sidebarUnfoldable }));
+  };
 
   return (
     <CSidebar
       className="border-end"
       colorScheme="dark"
       position="fixed"
-      unfoldable={unfoldable}
+      unfoldable={sidebarUnfoldable}
       visible={sidebarShow}
-      onVisibleChange={(visible) => {
-        dispatch({ type: 'set', sidebarShow: visible })
-      }}
+      onVisibleChange={toggleSidebar} // Güncellendi
     >
-      <CSidebarHeader className="border-bottom">
+      <CSidebarHeader className="border-bottom d-flex align-items-center">
         <CSidebarBrand to="/">
           <CIcon customClassName="sidebar-brand-full" icon={logo} height={32} />
           <CIcon customClassName="sidebar-brand-narrow" icon={sygnet} height={32} />
@@ -42,17 +47,17 @@ const AppSidebar = () => {
         <CCloseButton
           className="d-lg-none"
           dark
-          onClick={() => dispatch({ type: 'set', sidebarShow: false })}
+          onClick={() => toggleSidebar(false)} // Güncellendi
         />
       </CSidebarHeader>
+      
       <AppSidebarNav items={navigation} />
+
       <CSidebarFooter className="border-top d-none d-lg-flex">
-        <CSidebarToggler
-          onClick={() => dispatch({ type: 'set', sidebarUnfoldable: !unfoldable })}
-        />
+        <CSidebarToggler onClick={toggleUnfoldable} /> {/* Güncellendi */}
       </CSidebarFooter>
     </CSidebar>
-  )
-}
+  );
+};
 
-export default React.memo(AppSidebar)
+export default React.memo(AppSidebar);
--- src/store.js
+++ src/store.js
@@ -1,18 +1,46 @@
-import { legacy_createStore as createStore } from 'redux'
+// import { legacy_createStore as createStore } from 'redux'
 
+// const initialState = {
+//   sidebarShow: true,
+//   theme: 'light',
+// }
+
+// const changeState = (state = initialState, { type, ...rest }) => {
+//   switch (type) {
+//     case 'set':
+//       return { ...state, ...rest }
+//     default:
+//       return state
+//   }
+// }
+
+// const store = createStore(changeState)
+// export default store
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+
+// Başlangıç state'i
 const initialState = {
   sidebarShow: true,
   theme: 'light',
-}
+};
 
-const changeState = (state = initialState, { type, ...rest }) => {
-  switch (type) {
-    case 'set':
-      return { ...state, ...rest }
-    default:
-      return state
-  }
-}
+// Slice oluşturma
+const appSlice = createSlice({
+  name: 'app',
+  initialState,
+  reducers: {
+    setState: (state, action) => {
+      return { ...state, ...action.payload };
+    },
+  },
+});
 
-const store = createStore(changeState)
-export default store
+// Redux Store'u oluştur
+const store = configureStore({
+  reducer: {
+    app: appSlice.reducer,
+  },
+});
+
+export const { setState } = appSlice.actions;
+export default store;
