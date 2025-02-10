a701f0aa0f9eca1c52fc65187b3888f7dfc6981d
arif2004js <yearif689@gmail.com>
2025-02-10 15:13:13 +0400

    The Login page is separated from the Registration page

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React, { lazy } from 'react'
 
-const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
-const Login = React.lazy(() => import('./views/login/Login'))
-const TaskTable = React.lazy(()=>import('./views/TaskTable/TaskTable'))
+const Dashboard = lazy(() => import('./views/dashboard/Dashboard'))
+const TaskTable = lazy(() => import('./views/TaskTable/TaskTable'))
+const Register = lazy(() => import('./views/register/Register'))
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', element: Dashboard },
-  { path: '/pages/login', name: 'Login', element: Login },
-  { path: '/pages/task-table', name: 'TaskTable', element: TaskTable },
+  { path: '/pages/task-table', name: 'Task Table', element: TaskTable },
+  { path: '/pages/register', name: "Register" , element : Register}
 
 ]
 
